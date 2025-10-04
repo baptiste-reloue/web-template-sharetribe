16430b7bb366e082a3b3e9a9a83861af36e82042
baptiste-reloue <baptiste@reloue.fr>
2025-10-04 12:29:49 +0200

    Update EditListingWizard.js

--- src/containers/EditListingPage/EditListingWizard/EditListingWizard.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingWizard.js
@@ -235,7 +235,7 @@ const tabCompleted = (tab, listing, config) => {
       return !!price;
       
     case DEPOSIT:
-      return !!publicData?.depositNote;
+      return !!publicData?.deposit;
       
     case DELIVERY:
       return !!deliveryOptionPicked;
