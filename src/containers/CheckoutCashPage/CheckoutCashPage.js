42dc3049b50e867e2e0c8284f5e341afebaa5b90
baptiste-reloue <baptiste@reloue.fr>
2025-10-26 19:13:48 +0100

    Delete src/containers/CheckoutCashPage directory

--- src/containers/CheckoutCashPage/CheckoutCashPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import CheckoutPage from '../CheckoutPage/CheckoutPage';
-// CASH = même page, mais on force le process cash et on masque Stripe
-const CheckoutCashPage = props => <CheckoutPage {...props} forcedPaymentMethod="cash" />;
-export default CheckoutCashPage;
