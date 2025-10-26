20430730750c87f2c41d2afae1688ecb8f809f3c
baptiste-reloue <baptiste@reloue.fr>
2025-10-26 19:13:57 +0100

    Delete src/containers/CheckoutStripePage directory

--- src/containers/CheckoutStripePage/CheckoutStripePage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import CheckoutPage from '../CheckoutPage/CheckoutPage';
-// Stripe = process par défaut
-const CheckoutStripePage = props => <CheckoutPage {...props} forcedPaymentMethod="stripe" />;
-export default CheckoutStripePage;
