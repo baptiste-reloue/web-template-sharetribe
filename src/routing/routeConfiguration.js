9eb5a1777889efd6754c0144b366abfece7dee69
baptiste-reloue <baptiste@reloue.fr>
2025-11-01 23:56:48 +0100

    Update routeConfiguration.js

--- src/routing/routeConfiguration.js
+++ src/routing/routeConfiguration.js
@@ -14,6 +14,7 @@ const pageDataLoadingAPI = getPageDataLoadingAPI();
 
 const AuthenticationPage = loadable(() => import(/* webpackChunkName: "AuthenticationPage" */ '../containers/AuthenticationPage/AuthenticationPage'));
 const CheckoutPage = loadable(() => import(/* webpackChunkName: "CheckoutPage" */ '../containers/CheckoutPage/CheckoutPage'));
+const CheckoutCashPage = loadable(() => import(/* webpackChunkName: "CheckoutCashPage" */ '../containers/CheckoutCashPage/CheckoutCashPage'));
 const CMSPage = loadable(() => import(/* webpackChunkName: "CMSPage" */ '../containers/CMSPage/CMSPage'));
 const ContactDetailsPage = loadable(() => import(/* webpackChunkName: "ContactDetailsPage" */ '../containers/ContactDetailsPage/ContactDetailsPage'));
 const EditListingPage = loadable(() => import(/* webpackChunkName: "EditListingPage" */ '../containers/EditListingPage/EditListingPage'));
@@ -120,6 +121,13 @@ const routeConfiguration = (layoutConfig, accessControlConfig) => {
       auth: true,
       component: CheckoutPage,
       setInitialValues: pageDataLoadingAPI.CheckoutPage.setInitialValues,
+    },
+    {
+      path: '/l/:slug/:id/checkout-cash',
+      name: 'CheckoutCashPage',
+      auth: true,
+      component: CheckoutCashPage,
+      setInitialValues: pageDataLoadingAPI.CheckoutCashPage.setInitialValues,
     },
     {
       path: '/l/:slug/:id/:variant',
