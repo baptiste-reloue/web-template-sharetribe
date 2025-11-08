48fe4681aa1fc8f1dbdbc7c05c39cb0e64db8881
baptiste-reloue <baptiste@reloue.fr>
2025-11-08 14:25:12 +0100

    Update routeConfiguration.js

--- src/routing/routeConfiguration.js
+++ src/routing/routeConfiguration.js
@@ -127,7 +127,6 @@ const routeConfiguration = (layoutConfig, accessControlConfig) => {
       name: 'CheckoutCashPage',
       auth: true,
       component: CheckoutCashPage,
-      setInitialValues: pageDataLoadingAPI.CheckoutPage.setInitialValues,
     },
     {
       path: '/l/:slug/:id/:variant',
